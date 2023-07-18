9ee73a58de6c019c533fb770956dad3fa8e0e46a
Sophie-Ndobe <ndobes76@gmail.com>
2023-07-18 12:40:37 +0200

    Added date code in JavaScript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+let date = new Date();
 
+let hours = date.getHours();
+
+if (hours < 10) {
+  let hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes();
+
+if (hours < 10) {
+  let minutes = `0${minutes}`;
+}
+
+let currentTime = document.querySelector("#time");
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[date.getDay()];
+
+let currentDay = document.querySelector("#day");
+currentDay.innerHTML = `${day}`;
