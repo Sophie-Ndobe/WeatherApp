970913b54607059a346cb8b90cdec109c78cc759
Sophie-Ndobe <ndobes76@gmail.com>
2023-07-18 21:54:14 +0200

    Updated HTML CSS and JS code details

--- src/app.js
+++ src/app.js
@@ -33,9 +33,8 @@ currentDay.innerHTML = `${day}`;
 function searchCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city").value;
-  // console.log(cityInput);
   let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=metric`;
 
   let heading = document.querySelector("h1");
   heading.innerHTML = `${cityInput}`;
@@ -49,6 +48,9 @@ function displayWeather(response) {
   let windSpeedValue = Math.round(response.data.wind.speed);
   let descriptionValue = response.data.condition.description;
 
+  // let iconValue = response.data.condition.icon;
+  // let iconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${iconValue}.png`;
+
   let temperatureUpdate = document.querySelector("#temperature");
   temperatureUpdate.innerHTML = `${temperatureValue}`;
 
@@ -60,6 +62,9 @@ function displayWeather(response) {
 
   let descriptionUpdate = document.querySelector("#description");
   descriptionUpdate.innerHTML = `${descriptionValue}`;
+
+  //let iconUpdate = document.querySelector("#weather-icon");
+  //iconUpdate.innerHTML = `${iconValue}`;
 }
 
 let searchForm = document.querySelector("#form");
