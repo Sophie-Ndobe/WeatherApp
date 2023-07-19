6deed79376f013a794ea1e7b333cf6577e2ae5cc
Sophie-Ndobe <ndobes76@gmail.com>
2023-07-19 13:44:09 +0200

    Updated code and added a conversion factor for temperature

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ let date = new Date();
 let hours = date.getHours();
 
 if (hours < 10) {
-  let hours = `0${hours}`;
+  let updatedHours = `0${hours}`;
 }
 
 let minutes = date.getMinutes();
 
-if (hours < 10) {
-  let minutes = `0${minutes}`;
+if (minutes < 10) {
+  let updatedMinutes = `${minutes}`;
 }
 
 let currentTime = document.querySelector("#time");
@@ -30,42 +30,51 @@ let day = days[date.getDay()];
 let currentDay = document.querySelector("#day");
 currentDay.innerHTML = `${day}`;
 
-function searchCity(event) {
-  event.preventDefault();
+function searchCity() {
   let cityInput = document.querySelector("#city").value;
+
   let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=metric`;
-
-  let heading = document.querySelector("h1");
-  heading.innerHTML = `${cityInput}`;
 
   axios.get(`${apiUrl}`).then(displayWeather);
 }
 
 function displayWeather(response) {
-  let temperatureValue = Math.round(response.data.temperature.current);
+  let headingUpdate = document.querySelector("h1");
+  let temperatureUpdate = document.querySelector("#temperature");
+  let humidityUpdate = document.querySelector("#humidity");
+  let windSpeedUpdate = document.querySelector("#windspeed");
+  let descriptionUpdate = document.querySelector("#description");
+
+  celciusTemperature = response.data.temperature.current;
+
+  let headingValue = response.data.city;
+  let temperatureValue = Math.round(celciusTemperature);
   let humidityValue = response.data.temperature.humidity;
   let windSpeedValue = Math.round(response.data.wind.speed);
   let descriptionValue = response.data.condition.description;
 
-  // let iconValue = response.data.condition.icon;
-  // let iconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${iconValue}.png`;
+  headingUpdate.innerHTML = `${headingValue}`;
+  temperatureUpdate.innerHTML = `${temperatureValue}`;
+  humidityUpdate.innerHTML = `${humidityValue}`;
+  windSpeedUpdate.innerHTML = `${windSpeedValue}`;
+  descriptionUpdate.innerHTML = `${descriptionValue}`;
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
 
   let temperatureUpdate = document.querySelector("#temperature");
-  temperatureUpdate.innerHTML = `${temperatureValue}`;
-
-  let humidityUpdate = document.querySelector("#humidity");
-  humidityUpdate.innerHTML = `${humidityValue}`;
-
-  let windSpeedUpdate = document.querySelector("#windspeed");
-  windSpeedUpdate.innerHTML = `${windSpeedValue}`;
-
-  let descriptionUpdate = document.querySelector("#description");
-  descriptionUpdate.innerHTML = `${descriptionValue}`;
-
-  //let iconUpdate = document.querySelector("#weather-icon");
-  //iconUpdate.innerHTML = `${iconValue}`;
+  temperatureUpdate.innerHTML = Math.round(fahrenheitTemperature);
 }
+
+let celciusTemperature = null;
 
 let searchForm = document.querySelector("#form");
 searchForm.addEventListener("submit", searchCity);
+
+let fahrenheitConversion = document.querySelector("#fahrenheit-unit");
+fahrenheitConversion.addEventListener("click", convertToFahrenheit);
+
+searchCity("Paris");
